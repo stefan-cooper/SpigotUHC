c6927427905781a8fbf0a4a2c96f82ac84b4e50e
JawadJ <Jawad_jamil@hotmail.co.uk>
2024-09-14 12:09:09 +0100

    Changes for some more of the initital setup of the start

--- src/main/java/com/stefancooper/SpigotUHC/ConfigParser.java
+++ src/main/java/com/stefancooper/SpigotUHC/ConfigParser.java
@@ -2,6 +2,7 @@ package com.stefancooper.SpigotUHC;
 
 import com.stefancooper.SpigotUHC.types.Configurable;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.WorldBorder;
 
 import java.util.List;
@@ -20,6 +21,12 @@ public class ConfigParser {
     public Configurable<?> propertyToConfigurable(String key, String value) {
         return switch (fromString(key)) {
             case WORLD_BORDER_INITIAL_SIZE -> new Configurable<>(WORLD_BORDER_INITIAL_SIZE, Double.parseDouble(value));
+            case WORLD_BORDER_FINAL_SIZE -> new Configurable<>(WORLD_BORDER_FINAL_SIZE, Double.parseDouble(value));
+            case WORLD_BORDER_SHRINKING_PERIOD -> new Configurable<>(WORLD_BORDER_SHRINKING_PERIOD, Double.parseDouble(value));
+            case WORLD_BORDER_GRACE_PERIOD -> new Configurable<>(WORLD_BORDER_GRACE_PERIOD, Double.parseDouble(value));
+            case WORLD_BORDER_CENTER_X -> new Configurable<>(WORLD_BORDER_CENTER_X, Double.parseDouble(value));
+            case WORLD_BORDER_CENTER_Y -> new Configurable<>(WORLD_BORDER_CENTER_Y, Double.parseDouble(value));
+            case WORLD_BORDER_CENTER_Z -> new Configurable<>(WORLD_BORDER_CENTER_Z, Double.parseDouble(value));
             case WORLD_NAME -> new Configurable<>(WORLD_NAME, value);
             case null -> null;
 
@@ -36,6 +43,21 @@ public class ConfigParser {
                 Double newWorldBorderSize = (Double) configurable.value();
                 WorldBorder worldBorder = Bukkit.getWorld(config.getProp(WORLD_NAME.configName)).getWorldBorder();
                 worldBorder.setSize(newWorldBorderSize);
+                break;
+            case WORLD_BORDER_CENTER_X:
+                Double newWorldBorderX = (Double) configurable.value();
+                Double worldCenterX = Bukkit.getWorld(config.getProp(WORLD_NAME.configName)).getWorldBorder().getCenter().getX();
+                newWorldBorderX = worldCenterX;
+                break;
+            case WORLD_BORDER_CENTER_Y:
+                Double newWorldBorderY = (Double) configurable.value();
+                Double worldCenterY = Bukkit.getWorld(config.getProp(WORLD_NAME.configName)).getWorldBorder().getCenter().getY();
+                newWorldBorderY = worldCenterY;
+                break;
+            case WORLD_BORDER_CENTER_Z:
+                Double newWorldBorderZ = (Double) configurable.value();
+                Double worldCenterZ = Bukkit.getWorld(config.getProp(WORLD_NAME.configName)).getWorldBorder().getCenter().getZ();
+                newWorldBorderZ = worldCenterZ;
                 break;
             default:
                 break;
--- src/main/java/com/stefancooper/SpigotUHC/resources/ConfigKey.java
+++ src/main/java/com/stefancooper/SpigotUHC/resources/ConfigKey.java
@@ -2,6 +2,12 @@ package com.stefancooper.SpigotUHC.resources;
 
 public enum ConfigKey {
     WORLD_BORDER_INITIAL_SIZE("world.border.initial.size"),
+    WORLD_BORDER_FINAL_SIZE("world.border.final.size"),
+    WORLD_BORDER_SHRINKING_PERIOD("world.border.shrinking.period"),
+    WORLD_BORDER_GRACE_PERIOD("world.border.grace.period"),
+    WORLD_BORDER_CENTER_X("world.border.center.x"),
+    WORLD_BORDER_CENTER_Y("world.border.center.y"),
+    WORLD_BORDER_CENTER_Z("world.border.center.z"),
     WORLD_NAME("world.name");
 
     public final String configName;

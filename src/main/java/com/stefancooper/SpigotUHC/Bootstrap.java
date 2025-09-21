1c18f591f25afe50c235c1aba46873e6d9f0a243
Stefan Cooper <stefan.cooper27@gmail.com>
2025-09-21 01:53:21 +0100

    feat: add blastwave enchantment

--- src/main/java/com/stefancooper/SpigotUHC/Bootstrap.java
+++ src/main/java/com/stefancooper/SpigotUHC/Bootstrap.java
@@ -4,13 +4,16 @@ import com.stefancooper.SpigotUHC.utils.Constants;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry;
+import io.papermc.paper.registry.event.RegistryComposeEvent;
 import io.papermc.paper.registry.event.RegistryEvents;
 import io.papermc.paper.registry.keys.EnchantmentKeys;
 import io.papermc.paper.registry.keys.tags.ItemTypeTagKeys;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemType;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,23 +27,28 @@ public class Bootstrap implements PluginBootstrap {
         context.getLifecycleManager().registerEventHandler(
         RegistryEvents.ENCHANTMENT.compose().newHandler(event -> {
             try {
-                final String quickboomEnchantmentKey =  String.format("%s:%s", Constants.SPIGOT_NAMESPACE, Constants.QUICKBOOM_ENCHANTMENT);
-                var enchantKey = EnchantmentKeys.create(Key.key(quickboomEnchantmentKey));
-                event.registry().register(
-                        enchantKey,
-                        b -> b.description(Component.text("Quickboom"))
-                                .supportedItems(event.getOrCreateTag(ItemTypeTagKeys.NOTEBLOCK_TOP_INSTRUMENTS))
-                                .anvilCost(1)
-                                .maxLevel(3)
-                                .weight(10)
-                                .minimumCost(EnchantmentRegistryEntry.EnchantmentCost.of(1, 1))
-                                .maximumCost(EnchantmentRegistryEntry.EnchantmentCost.of(3, 1))
-                                .activeSlots(EquipmentSlotGroup.HAND)
-                );
-                context.getLogger().debug("Quickboom enchantment registered successfully!");
+                final TypedKey<Enchantment> quickboomKey = EnchantmentKeys.create(Key.key(String.format("%s:%s", Constants.SPIGOT_NAMESPACE, Constants.QUICKBOOM_ENCHANTMENT)));
+                final TypedKey<Enchantment> blastwaveKey = EnchantmentKeys.create(Key.key(String.format("%s:%s", Constants.SPIGOT_NAMESPACE, Constants.BLASTWAVE_ENCHANTMENT)));
+                registerTNTEnchantment(event, quickboomKey, "Quickboom", 4);
+                registerTNTEnchantment(event, blastwaveKey, "Blastwave", 4);
+                context.getLogger().debug("Custom enchantments registered successfully!");
             } catch (Throwable t) {
-                context.getLogger().error("Failed to register custom enchantment", t);
+                context.getLogger().error("Failed to register custom enchantments", t);
             }
         }));
+    }
+
+    private void registerTNTEnchantment(final RegistryComposeEvent<Enchantment, EnchantmentRegistryEntry.Builder> event, final TypedKey<Enchantment> key, final String label, final int maxLevel) {
+        event.registry().register(
+                key,
+                b -> b.description(Component.text(label))
+                        .supportedItems(event.getOrCreateTag(ItemTypeTagKeys.NOTEBLOCK_TOP_INSTRUMENTS))
+                        .anvilCost(1)
+                        .maxLevel(maxLevel)
+                        .weight(10)
+                        .minimumCost(EnchantmentRegistryEntry.EnchantmentCost.of(1, 1))
+                        .maximumCost(EnchantmentRegistryEntry.EnchantmentCost.of(maxLevel, 1))
+                        .activeSlots(EquipmentSlotGroup.HAND)
+        );
     }
 }
